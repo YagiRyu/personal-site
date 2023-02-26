4baa875c4006b26c1f81348c44ab743ba9160217
Ryutaro Yagi <ryucode822@gmail.com>
2023-02-27 01:07:26 +0900

    Added coming soon content

--- src/jsMain/kotlin/pages/PageNotFound.kt
+++ src/jsMain/kotlin/pages/PageNotFound.kt
@@ -1,9 +1,29 @@
 package pages
 
 import androidx.compose.runtime.Composable
+import org.jetbrains.compose.web.css.*
+import org.jetbrains.compose.web.dom.Div
+import org.jetbrains.compose.web.dom.Span
 import org.jetbrains.compose.web.dom.Text
 
 @Composable
 fun PageNotFound() {
-    Text("Hello")
+    Div({
+        style {
+            display(DisplayStyle.Flex)
+            justifyContent(JustifyContent.Center)
+            alignItems(AlignItems.Center)
+            height(90.vh)
+            property("margin-top", "auto")
+            property("margin-bottom", "auto")
+        }
+    }) {
+        Span({
+            style {
+                fontSize(50.px)
+            }
+        }) {
+            Text("Page not found.....\uD83E\uDD72")
+        }
+    }
 }
--- src/jsMain/kotlin/pages/Project.kt
+++ src/jsMain/kotlin/pages/Project.kt
@@ -1,9 +1,29 @@
 package pages
 
 import androidx.compose.runtime.Composable
+import org.jetbrains.compose.web.css.*
+import org.jetbrains.compose.web.dom.Div
+import org.jetbrains.compose.web.dom.Span
 import org.jetbrains.compose.web.dom.Text
 
 @Composable
 fun Project() {
-    Text("Project page")
+    Div({
+        style {
+            display(DisplayStyle.Flex)
+            justifyContent(JustifyContent.Center)
+            alignItems(AlignItems.Center)
+            height(90.vh)
+            property("margin-top", "auto")
+            property("margin-bottom", "auto")
+        }
+    }) {
+        Span({
+            style {
+                fontSize(50.px)
+            }
+        }) {
+            Text("Coming soon.....\uD83D\uDE80")
+        }
+    }
 }
